466c6b9d11827d8d8b3fe2335e50f163c7205920
unknown <bons6710hos@gmail.com>
2025-08-12 20:38:14 +0300

    Creating Blog Post Management Features

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import reverse_lazy
 from .forms import CustomUserCreationForm
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
@@ -32,36 +33,55 @@ def profile_view(request):
         return redirect('profile')
     return render(request, 'blog/profile.html')
 
+# Post Views
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
     ordering = ['-published_date']
+    # paginate_by = 5
+
+# Post Detail
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
+# Post Create
 class PostCreateView(CreateView):
     model = Post
     template_name = 'blog/post_form.html'
-    fields = ['title', 'content', 'published_date']
-    success_url = '/'
-
+    fields = ['title', 'content']
+    def get_success_url(self):
+        return reverse_lazy('post-list') 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    #post update
 class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
     model = Post
     template_name = 'blog/post_form.html'
-    fields = ['title', 'content', 'published_date']
-    success_url = '/'
+    fields = ['title', 'content', ]
+    def get_success_url(self):
+        return reverse_lazy('post-list') 
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    def test_func(self):
+        """Only allow the author to update the post."""
+        post = self.get_object()
+        return self.request.user == post.author
+    
+# Post Delete
 class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
-    success_url = '/'
+    def get_success_url(self):
+        return reverse_lazy('post-list') 
+    def test_func(self):
+        """Only allow the author to delete the post."""
+        post = self.get_object()
+        return self.request.user == post.author
 
 
