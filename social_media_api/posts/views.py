a9afe6884dbcfd1e80c0cecdd9df7a234d91e747
unknown <bons6710hos@gmail.com>
2025-08-21 21:03:05 +0300

    Implementing Notifications and Likes Functionality

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status,generics
+from notifications.models import Notification
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
